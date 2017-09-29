f7374655d697045f43fbe6ca1bf9256beabd4e33
Tyquan Reddick <Tjreddick@gmail.com>
2017-09-28 21:03:48 -0400

    updated password helper

--- init.py
+++ init.py
@@ -13,9 +13,12 @@ from flask_login import LoginManager
 from flask_login import login_required
 from flask_login import login_user
 
-
+from passwordhelper import PasswordHelper
 from mockdbhelper import MockDBHelper as DBHelper
 from user import User
+
+PH = PasswordHelper()
+DB = DBHelper()
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
@@ -27,16 +30,32 @@ app.secret_key = 'pPXJA3C37Qybz4QykP+hOyUvVQeEXk1Ao4C8upz+fGQXKsM'
 def index():
     return render_template("index.html")
 
+# sign a user up
+@app.route("/register", methods=["POST"])
+def register():
+	email = request.form.get("email")
+	pw1 = request.form.get("password")
+	pw2 = request.form.get("password2")
+	if not pw1 == pw2:
+		return redirect(url_for('index'))
+	if DB.get_user(email):
+		return redirect(url_for('index'))
+	salt = PH.get_salt()
+	hashed = PH.get_hash(pw1 + salt)
+	DB.add_user(email, salt, hashed)
+	return redirect(url_for('index'))
+
 #Log user in route
 @app.route("/login", methods=["POST"])
 def login():
 	email = request.form.get("email")
 	password = request.form.get("password")
-	user_password = DB.get_user(email)
-	if user_password and user_password == password:
+	stored_user = DB.get_user(email)
+	if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
 		user = User(email)
 		login_user(user, remember=True)
 		return redirect(url_for('account'))
+	return index()
 
 #load user
 @login_manager.user_loader
